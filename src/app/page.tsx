6c0a8d884209a79b528fffe29dd3f20e7cd5356c
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-04-17 16:03:08 +0530

    add more components and write some tailwind configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import Hero from "@/components/Hero";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
 	return (
 		<main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
 			<Hero />
+			<FeaturedCourses />
+			<WhyChooseUs />
+			<TestimonialCards />
 		</main>
 	);
 }
