83b8fe2d0e94ed8abc94394d5887dfa83daed857
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-04-18 06:01:59 +0530

    "Add Simple Noise package, update next.config.mjs to allow images from unsplash, update Home page to include UpcomingWebinars, Instructors, and Footer components"

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -37,7 +37,7 @@ const musicSchoolTestimonials = [
 function TestimonialCards() {
 	return (
 		<div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.1] relative flex flex-col items-center justify-center overflow-hidden">
-			<h2 className="text-3xl font-bold text-center mb-8 z-10">
+			<h2 className="text-3xl font-bold text-center mb-8 z-10 text-gray-400">
 				Hear our Harmony: Voices of success
 			</h2>
 			<div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
